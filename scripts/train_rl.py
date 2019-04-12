dde3780ececa9677e0758a51f8d363747d028930
taungerine <10890291+taungerine@users.noreply.github.com>
2019-04-12 22:24:37 +0200

    Added --disc-comm argument

--- scripts/train_rl.py
+++ scripts/train_rl.py
@@ -54,6 +54,8 @@ parser.add_argument("--no-comm", action="store_true", default=False,
                     help="don't use communication")
 parser.add_argument("--all-angles", action="store_true", default=False,
                     help="let the sender observe the environment from all angles")
+parser.add_argument("--disc-comm", action="store_true", default=False,
+                    help="use discrete instead of continuous communication")
 args = parser.parse_args()
 
 utils.seed(args.seed)
@@ -110,7 +112,7 @@ if acmodel0 is None:
         acmodel0 = ACModel(obss_preprocessor.obs_space, envs0[0].action_space,
                            args.image_dim, args.memory_dim, args.instr_dim, args.enc_dim, args.dec_dim,
                            not args.no_instr, args.instr_arch, not args.no_mem, args.arch,
-                           args.len_message, args.num_symbols, args.all_angles)
+                           args.len_message, args.num_symbols, args.all_angles, args.disc_comm)
 if acmodel1 is None:
     if args.pretrained_model:
         acmodel1 = utils.load_model(args.pretrained_model, 1, raise_not_found=True)
@@ -119,7 +121,7 @@ if acmodel1 is None:
         acmodel1 = ACModel(obss_preprocessor.obs_space, envs1[0].action_space,
                            args.image_dim, args.memory_dim, args.instr_dim, args.enc_dim, args.dec_dim,
                            not args.no_instr, args.instr_arch, not args.no_mem, args.arch,
-                           args.len_message, args.num_symbols)
+                           args.len_message, args.num_symbols, False, args.disc_comm)
 
 obss_preprocessor.vocab.save()
 utils.save_model(acmodel0, args.model, 0)
