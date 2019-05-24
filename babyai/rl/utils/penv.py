1424b84f50b7e326bc968891b79974f07ae4668a
taungerine <10890291+taungerine@users.noreply.github.com>
2019-05-24 03:25:06 +0200

    Added --frequency argument

--- babyai/rl/utils/penv.py
+++ babyai/rl/utils/penv.py
@@ -33,99 +33,85 @@ def get_local(obs):
     # get local view
     return obs['image'][3:4, 5:7, :]
 
-def worker(conn, env0, env1):
+def adapt(results, scouting):
+    results = results.copy()
+    i = 0
+    for scouting_ in scouting:
+        if scouting_:
+            results[i]    = list(results[i])
+            results[i][2] = 0.0
+            results[i][3] = True
+            results[i]    = tuple(results[i])
+        i += 1
+    return results
+
+def worker(conn, env):
     while True:
-        cmd, scouting, action = conn.recv()
+        cmd, data = conn.recv()
         if cmd == "step":
-            if scouting:
-                obs, reward, done, info = env0.step(action)
-                obs   = env1.reset()
-                globs = obs.copy()
-                obs['image']   = get_local(obs)
-                globs['image'] = get_global(env1)
-                done = True
-                reward *= 0
-            else:
-                obs, reward, done, info = env1.step(action)
-                if done:
-                    obs   = env0.reset()
-                    globs = obs.copy()
-                    obs['image']   = get_local(obs)
-                    globs['image'] = get_global(env0)
-                else:
-                    globs = obs.copy()
-                    obs['image']   = get_local(obs)
-                    globs['image'] = get_global(env1)
-            conn.send((globs, obs, reward, done, info))
-        elif cmd == "reset":
-            if scouting:
-                env1.reset()
-            obs   = env0.reset()
+            obs, reward, done, info = env.step(data)
+            if done:
+                obs = env.reset()
             globs = obs.copy()
             obs['image']   = get_local(obs)
-            globs['image'] = get_global(env0)
-            conn.send((globs, obs))
+            globs['image'] = get_global(env)
+            step_count = env.step_count
+            conn.send((globs, obs, reward, done, step_count))
+        elif cmd == "reset":
+            obs = env.reset()
+            globs = obs.copy()
+            obs['image']   = get_local(obs)
+            globs['image'] = get_global(env)
+            conn.send((globs, obs, 0.0, False, 0))
         else:
             raise NotImplementedError
 
 class ParallelEnv(gym.Env):
     """A concurrent execution of environments in multiple processes."""
 
-    def __init__(self, envs0, envs1):
-        assert len(envs0) >= 1, "No environment given."
+    def __init__(self, envs):
+        assert len(envs) >= 1, "No environment given."
 
-        self.envs0 = envs0
-        self.envs1 = envs1
-        self.observation_space = self.envs0[0].observation_space
-        self.action_space = self.envs0[0].action_space
+        self.envs = envs
+        self.observation_space = self.envs[0].observation_space
+        self.action_space = self.envs[0].action_space
 
         self.locals = []
-        for env0, env1 in zip(self.envs0[1:], self.envs1[1:]):
+        for env in self.envs[1:]:
             local, remote = Pipe()
             self.locals.append(local)
-            p = Process(target=worker, args=(remote, env0, env1))
+            p = Process(target=worker, args=(remote, env))
             p.daemon = True
             p.start()
             remote.close()
 
     def reset(self, scouting):
-        for local, scouting_ in zip(self.locals, scouting[1:]):
-            local.send(("reset", scouting_, None))
-        if scouting[0]:
-            self.envs1[0].reset()
-        obs   = self.envs0[0].reset()
+        for local in self.locals:
+            local.send(("reset", None))
+        obs = self.envs[0].reset()
         globs = obs.copy()
         obs['image']   = get_local(obs)
-        globs['image'] = get_global(self.envs0[0])
-        results = zip(*[(globs, obs)] + [local.recv() for local in self.locals])
-        return results
+        globs['image'] = get_global(self.envs[0])
+        self.results = [(globs, obs, 0.0, False, 0)] + [local.recv() for local in self.locals]
+        return zip(*adapt(self.results, scouting))
 
     def step(self, actions, scouting):
         for local, action, scouting_ in zip(self.locals, actions[1:], scouting[1:]):
-            local.send(("step", scouting_, action))
-        
-        if scouting[0]:
-            obs, reward, done, info = self.envs0[0].step(actions[0])
-            obs   = self.envs1[0].reset()
+            if not scouting_:
+                local.send(("step", action))
+        if not scouting[0]:
+            obs, reward, done, info = self.envs[0].step(actions[0])
+            if done:
+                obs = self.envs[0].reset()
             globs = obs.copy()
             obs['image']   = get_local(obs)
-            globs['image'] = get_global(self.envs1[0])
-            done = True
-            reward *= 0
-        else:
-            obs, reward, done, info = self.envs1[0].step(actions[0])
-            if done:
-                obs   = self.envs0[0].reset()
-                globs = obs.copy()
-                obs['image']   = get_local(obs)
-                globs['image'] = get_global(self.envs0[0])
-            else:
-                globs = obs.copy()
-                obs['image']   = get_local(obs)
-                globs['image'] = get_global(self.envs1[0])
-
-        results = zip(*[(globs, obs, reward, done, info)] + [local.recv() for local in self.locals])
-        return results
+            globs['image'] = get_global(self.envs[0])
+            step_count = self.envs[0].step_count
+            self.results[0] = (globs, obs, reward, done, step_count)
+        for i in range(len(self.envs)-1):
+            if not scouting[i+1]:
+                self.results[i+1] = self.locals[i].recv()
+        return zip(*adapt(self.results, scouting))
 
     def render(self):
         raise NotImplementedError
